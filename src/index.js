c5faa380de0222bab193b91310baa7a6d266bd26
John Yi <john.yi1411@gmail.com>
2019-11-10 23:16:18 -0800

    Add redux devtools in createStore as argument.

--- src/index.js
+++ src/index.js
@@ -40,7 +40,10 @@ const rootReducer = combineReducers({
   nameReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer, /* preloadedState, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
